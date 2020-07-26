ed1f226890effe81c9d4358c3101a8f14089f694
Mia-Cross <leila.marabese@gmail.com>
2020-07-26 18:59:19 +0200

    grafana : successfull import of datasources and dashboards

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,10 @@
 # nettoyer les fichiers de config
-kubectl delete -f srcs/mysql/mysql.yaml
-kubectl delete -f srcs/phpmyadmin/phpmyadmin.yaml
-kubectl delete -f srcs/wordpress/wordpress.yaml
+#kubectl delete -f srcs/mysql/mysql.yaml
+#kubectl delete -f srcs/phpmyadmin/phpmyadmin.yaml
+#kubectl delete -f srcs/wordpress/wordpress.yaml
 kubectl delete -f srcs/grafana/grafana.yaml
-kubectl delete -f srcs/influxdb/influxdb.yaml
-kubectl delete -f srcs/nginx/nginx.yaml
+#kubectl delete -f srcs/influxdb/influxdb.yaml
+#kubectl delete -f srcs/nginx/nginx.yaml
 
 # supprimer les services
 #kubectl delete services wordpress
--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ bash cleanup.sh
 
 ############################
 # load-balancer : MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-cp srcs/metallb/config_base.yaml srcs/metallb/config.yaml
-sed -ie "s/MINIKUBE_IP/$(minikube ip)/g" srcs/metallb/config.yaml
-kubectl apply -f srcs/metallb/config.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#cp srcs/metallb/config_base.yaml srcs/metallb/config.yaml
+#sed -ie "s/MINIKUBE_IP/$(minikube ip)/g" srcs/metallb/config.yaml
+#kubectl apply -f srcs/metallb/config.yaml
 
 ############################
 # faire le lien entre les docker daemons de minikube et de docker
@@ -20,31 +20,30 @@ eval $(minikube docker-env)
 
 ##########################
 # telegraf
-docker build -t my_telegraf srcs/telegraf/
-#kubectl apply -f srcs/telegraf/telegraf.yaml
+#docker build -t my_telegraf srcs/telegraf/
 ##########################
 # nginx
-docker build -t my_nginx srcs/nginx/
-kubectl apply -f srcs/nginx/nginx.yaml
+#docker build -t my_nginx srcs/nginx/
+#kubectl apply -f srcs/nginx/nginx.yaml
 ##########################
 # influxdb
-docker build -t my_influxdb srcs/influxdb/
-kubectl apply -f srcs/influxdb/influxdb.yaml
+#docker build -t my_influxdb srcs/influxdb/
+#kubectl apply -f srcs/influxdb/influxdb.yaml
 ##########################
 # grafana
 docker build -t my_grafana srcs/grafana/
 kubectl apply -f srcs/grafana/grafana.yaml
 ##########################
 # mysql
-docker build -t my_mysql srcs/mysql/
-kubectl apply -f srcs/mysql/mysql.yaml
+#docker build -t my_mysql srcs/mysql/
+#kubectl apply -f srcs/mysql/mysql.yaml
 ##########################
 # wordpress
-docker build -t my_wordpress srcs/wordpress/
-kubectl apply -f srcs/wordpress/wordpress.yaml
+#docker build -t my_wordpress srcs/wordpress/
+#kubectl apply -f srcs/wordpress/wordpress.yaml
 ##########################
 # phpmyadmin
-docker build -t my_phpmyadmin srcs/phpmyadmin/
-kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+#docker build -t my_phpmyadmin srcs/phpmyadmin/
+#kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 
 #screen -dmS dash minikube dashboard
